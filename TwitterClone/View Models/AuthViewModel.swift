1fc9c5f7318da6bb16db0b59c193135e1f3053fe
Chris <chris.sedano.arenas@gmail.com>
2021-01-28 09:26:33 +0000

    Register User
    in AuthViewModel.swift config funcregisterUser using Firebase and upload photo profile

--- TwitterClone/View Models/AuthViewModel.swift
+++ TwitterClone/View Models/AuthViewModel.swift
@@ -17,15 +17,46 @@ class AuthViewModel: ObservableObject {
     
     func registerUser(email: String, password: String, userName: String, fullName: String, profileImage: UIImage) {
         
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.2) else { return }
+        
+        let fileName = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child(fileName)
+        
+        storageRef.putData(imageData, metadata: nil) { _, error in
             
-            if let error = error {
-                // TO DO: I need an UIAlertController for any error
-                debugPrint("Error: \(error.localizedDescription)")
+            if let error =  error {
+                // TO DO: I need a UIAlertController
+                debugPrint("Error: failed to upload image \(error.localizedDescription)")
                 return
             }
+            print("DEBUG: Successfully upload user photo")
             
-            print("Success sign in new user")
+            storageRef.downloadURL { url, _ in
+                
+                guard let profileImageURL = url?.absoluteString else { return }
+                
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    
+                    if let error = error {
+                        // TO DO: I need an UIAlertController for any error
+                        debugPrint("Error: \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let user = result?.user else { return }
+                    
+                    let data = ["email": email,
+                                "userName": userName,
+                                "fullName": fullName,
+                                "profileImageURL": profileImageURL,
+                                "uid": user.uid]
+                    
+                    Firestore.firestore().collection("users").document(user.uid).setData(data) { _ in
+                        
+                        print("DEBUG: Successfully upload user data")
+                    }
+                }
+            }
         }
     }
 }
